bd30c26aff0af931ffa39fc366dbb0a460c6c696
Margarita Campos <MCampos1@martechmedical.com>
2021-12-20 17:01:52 -0800

    Button page

---
+++ Button/button.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./../views/_assets/css/styles.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet" />
+    <title>Button</title>
+</head>
+
+<body class="bg-success bg-gradient">
+    <main>
+        <!-- Main content container-->
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-xxl-7 col-xl-10">
+                    <div class="card card-raised shadow-10 mt-5 mt-xl-10 mb-5">
+                        <div class="card-body p-5">
+                            <!-- Auth header with logo image-->
+                            <div>
+                                <p class="mb-0">ITEM NUMBER</p>
+                                <h1 class="display-3 mb-0">
+                                    <strong>AC1005HF</strong>
+                                </h1>
+                                <h3 class="mb-5 mt-5">5,000/10,000</h3>
+                            </div>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-outline-success btn-xl" type="button">
+                                    <i class="leading-icon material-icons">verified_user</i>
+                                    Terminado
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
