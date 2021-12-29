27e8d1005e078456de622fcb23e6d67fa6ca5ec3
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-12-29 11:35:15 -0800

    Added PHP mailer

---
+++ libraries/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5bafd08eb8fa969971a95ddfe5a5e1e5b198274a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bafd08eb8fa969971a95ddfe5a5e1e5b198274a',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.3',
+      'version' => '6.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'baeb7cde6b60b1286912690ab0693c7789a31e71',
+    ),
+  ),
+);
